9acf6fb0c4f0ca31e5f8f303bbc44e391feec959
Mateo Algañarás <103078156+Tutealga@users.noreply.github.com>
2023-10-14 23:06:20 -0300

    Update readData.js

--- readData.js
+++ readData.js
@@ -4,7 +4,7 @@ import {get, ref, child} from "firebase/database"
 export const getProducts = async () => {
  const db = ref(rdb)
 
- const response = await get(child(db, '/'))
+ const response = await get(child(db, 'your_spread_sheet_id/spread_sheet_name'))
  const data = await response.val()
  return data
 }
