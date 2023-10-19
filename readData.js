fa887e233f78f0d52484758b351e40ea2cb4474e
Mateo Algañarás <103078156+Tutealga@users.noreply.github.com>
2023-10-18 23:34:44 -0300

    Update readData.js

--- readData.js
+++ readData.js
@@ -1,10 +1,27 @@
 import {rdb} from "../firebase/firebase"
 import {get, ref, child} from "firebase/database"
 
-export const getData = async () => {
+const getData = async () => {
  const db = ref(rdb)
 
  const response = await get(child(db, 'your_spread_sheet_id/spread_sheet_name'))
  const data = await response.val()
  return data
 }
+
+const Data = async () => {
+    const res = await getData();
+
+  return (
+    <section>
+  <article>
+  {
+      res.map(data => <Card key={data.id} data={data} />
+  }
+  </article>
+    </section>
+    )
+
+}
+
+export default Data
